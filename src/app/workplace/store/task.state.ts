4a9eacd26742f9d49b950de683bc53aeec2da99c
Name <Email>
2021-03-15 15:44:13 +0100

    added state subscriptions

--- src/app/workplace/store/task.state.ts
+++ src/app/workplace/store/task.state.ts
@@ -30,52 +30,60 @@ export class TaskState {
   constructor(private taskService: TaskService) {
   }
 
-  // TODO update tasks, after delete, update etc
-
   @Action(GetTasks)
   getTasks(ctx: StateContext<TaskStateModel>, action: GetTasks): Observable<Task[]> {
-    return this.taskService.getTasks(action.filter).pipe(take(1),
+    const obs = this.taskService.getTasks(action.filter).pipe(take(1),
       map(taskResource => (taskResource.tasks as Task[])),
       tap(tasks => ctx.patchState({tasks})));
+    obs.pipe(take(1)).subscribe();
+    return obs;
   }
 
   @Action(SelectTask)
   selectTask(ctx: StateContext<TaskStateModel>, action: SelectTask): Observable<Task> {
-    return this.taskService.getTaskById(action.taskId).pipe(take(1),
+    const obs = this.taskService.getTaskById(action.taskId).pipe(take(1),
       tap(selectedTask => ctx.patchState({
           selectedTask, creating: false, tasks: ctx.getState().tasks.map(task => {
             return task.taskId === selectedTask.taskId ? selectedTask : task;
           })
         })
       ));
+    obs.pipe(take(1)).subscribe();
+    return obs;
   }
 
   @Action(CompleteTask)
   completeTask(ctx: StateContext<TaskStateModel>): Observable<any> {
-    return this.taskService.completeTask(ctx.getState().selectedTask.taskId).pipe(take(1),
+    const obs = this.taskService.completeTask(ctx.getState().selectedTask.taskId).pipe(take(1),
       tap(selectedTask => ctx.patchState({
         selectedTask, tasks: ctx.getState().tasks.map(task => {
           return task.taskId === selectedTask.taskId ? selectedTask : task;
         })
       })));
+    obs.pipe(take(1)).subscribe();
+    return obs;
   }
 
   @Action(SaveTask)
   saveTask(ctx: StateContext<TaskStateModel>, action: SaveTask): Observable<any> {
     if (ctx.getState().creating) {
-      return this.taskService.createTask(action.task).pipe(take(1),
+      const obs = this.taskService.createTask(action.task).pipe(take(1),
         tap(selectedTask => ctx.patchState({
           selectedTask,
           creating: false,
           tasks: [...ctx.getState().tasks, selectedTask]
         })));
+      obs.pipe(take(1)).subscribe();
+      return obs;
     } else {
-      return this.taskService.updateTask(action.task).pipe(take(1),
+      const obs = this.taskService.updateTask(action.task).pipe(take(1),
         tap(selectedTask => ctx.patchState({
           selectedTask, creating: false, tasks: ctx.getState().tasks.map(task => {
             return task.taskId === selectedTask.taskId ? selectedTask : task;
           })
         })));
+      obs.pipe(take(1)).subscribe();
+      return obs;
     }
   }
 
@@ -88,40 +96,48 @@ export class TaskState {
   @Action(DeleteTask)
   deleteTask(ctx: StateContext<TaskStateModel>): Observable<any> {
     const state = ctx.getState();
-    return this.taskService.deleteTask(state.selectedTask.taskId).pipe(take(1),
+    const obs = this.taskService.deleteTask(state.selectedTask.taskId).pipe(take(1),
       tap(() => ctx.patchState({
         selectedTask: null,
         tasks: state.tasks.filter(task => task.taskId !== state.selectedTask.taskId)
       })));
+    obs.pipe(take(1)).subscribe();
+    return obs;
   }
 
   @Action(ClaimTask)
   claimTask(ctx: StateContext<TaskStateModel>): Observable<any> {
-    return this.taskService.claimTask(ctx.getState().selectedTask.taskId).pipe(take(1),
+    const obs = this.taskService.claimTask(ctx.getState().selectedTask.taskId).pipe(take(1),
       tap(selectedTask => ctx.patchState({
         selectedTask, tasks: ctx.getState().tasks.map(task => {
           return task.taskId === selectedTask.taskId ? selectedTask : task;
         })
       })));
+    obs.pipe(take(1)).subscribe();
+    return obs;
   }
 
   @Action(CancelClaim)
   cancelClaim(ctx: StateContext<TaskStateModel>): Observable<any> {
-    return this.taskService.cancelClaimTask(ctx.getState().selectedTask.taskId).pipe(take(1),
+    const obs = this.taskService.cancelClaimTask(ctx.getState().selectedTask.taskId).pipe(take(1),
       tap(selectedTask => ctx.patchState({
         selectedTask, tasks: ctx.getState().tasks.map(task => {
           return task.taskId === selectedTask.taskId ? selectedTask : task;
         })
       })));
+    obs.pipe(take(1)).subscribe();
+    return obs;
   }
 
   @Action(TransferTask)
   transferTask(ctx: StateContext<TaskStateModel>, action: TransferTask): Observable<any> {
-    return this.taskService.transferTask(ctx.getState().selectedTask.taskId, action.workbasketId).pipe(take(1),
+    const obs = this.taskService.transferTask(ctx.getState().selectedTask.taskId, action.workbasketId).pipe(take(1),
       tap(selectedTask => ctx.patchState({
         selectedTask, tasks: ctx.getState().tasks.map(task => {
           return task.taskId === selectedTask.taskId ? selectedTask : task;
         })
       })));
+    obs.pipe(take(1)).subscribe();
+    return obs;
   }
 }
